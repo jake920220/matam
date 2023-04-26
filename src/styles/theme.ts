b11b7f95b0f10cc54796a8eebb0561ade2199059
jake920220 <linokim220@gmail.com>
2023-04-26 18:47:11 +0900

    [chore] add styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const light = {
+    bgColor: "#fff",
+    textColor: "#000",
+};
+const dark = {
+    bgColor: "#121212",
+    textColor: "#ECECEC",
+};
+
+const theme = {
+    light,
+    dark,
+};
+
+export default theme;
