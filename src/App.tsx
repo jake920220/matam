409caf143a643fad755ea3441c6d63dae7b88331
jake920220 <linokim220@gmail.com>
2023-04-26 15:19:15 +0900

    [chore] webpack and typescript settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const App = () => {
+const App = (): JSX.Element => {
     return <h1>Hello world</h1>;
 };
 
---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const rules = [
+    {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+        },
+    },
+    {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "ts-loader",
+            options: {
+                configFile: path.resolve(__dirname, "tsconfig.json"),
+            },
+        },
+    },
+    {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg)$/,
+        loader: "url-loader",
+    },
+    {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+    },
+];
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, "src", "index.tsx"),
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: { rules },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+    ],
+};
