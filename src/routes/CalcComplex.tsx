677e077d42c21a1e56c0caf74149ec71a3abab41
Junhyun Kim <linokim220@gmail.com>
2023-05-08 18:58:09 +0900

    [feat] 상세계산페이지 개발중 처리

--- src/routes/CalcComplex.tsx
+++ src/routes/CalcComplex.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 
 const CalcComplex = () => {
     return (
-        <div>
-            <h2>😭</h2>
-        </div>
+        <ComplexContainer>
+            <EmojiIcon>😭</EmojiIcon>
+            <PageTitle>죄송합니다.</PageTitle>
+            <PageText>개발 준비중입니다.</PageText>
+        </ComplexContainer>
     );
 };
+
+const ComplexContainer = styled.article`
+    min-height: 400px;
+    padding: 8px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const EmojiIcon = styled.span`
+    display: block;
+    font-size: 80px;
+`;
+
+const PageTitle = styled.h2`
+    margin-top: 24px;
+    font-size: 28px;
+`;
+
+const PageText = styled.p`
+    margin-top: 16px;
+    font-size: 16px;
+`;
 
 export default CalcComplex;
